d88a32f903cf3c90f44b88d539fc0c159d84e355
FelipeAmorimFerreira <felipe.amorim.ferreira@ccc.ufcg.edu.br>
2021-11-04 01:02:39 -0300

    Bridge pattern

---
+++ structural/bridge/example.py
@@ -0,0 +1,54 @@
+"""
+Bridge Design Pattern
+
+Motivation:
+ - Bridge prevents a "Cartesian product" complexity explosion
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Renderer(ABC):
+
+    @abstractmethod
+    def render_circle(self, radius):
+        pass
+
+
+class VectorRenderer(Renderer):
+    def render_circle(self, radius):
+        print(f'Drawing a circle of radius {radius}')
+
+
+class RasterRenderer(Renderer):
+    def render_circle(self, radius):
+        print(f"Drawing pixelsf for circle of radius {radius}")
+
+
+class Shape:
+    def __init__(self, renderer):
+        self.renderer = renderer
+
+    def draw(self): pass
+    def resize(self, factor): pass
+
+
+class Circle(Shape):
+    def __init__(self, renderer, radius):
+        super().__init__(renderer)
+        self.radius = radius
+
+    def draw(self):
+        self.renderer.render_circle(self.radius)
+
+    def resize(self, factor):
+        self.radius *= factor
+
+
+if __name__ == "__main__":
+    raster = RasterRenderer()
+    vector = VectorRenderer()
+    circle = Circle(vector, 5)
+    circle.draw()
+    circle.resize(2)
+    circle.draw()
