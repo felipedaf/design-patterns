2a92a10e01d241e07cd46da33eb2e4e0a707532e
FelipeAmorimFerreira <felipe.amorim.ferreira@ccc.ufcg.edu.br>
2021-11-05 05:28:11 -0300

    Observer design pattern

---
+++ behavioral/observer/example.py
@@ -0,0 +1,26 @@
+class Event(list):
+    def __call__(self, *args, **kwargs):
+        for item in self:
+            item(*args, **kwargs)
+
+class Person:
+    def __init__(self, name, address) -> None:
+        self.name = name
+        self.address = address
+        self.falls_ill = Event()
+
+
+    def catch_a_cold(self):
+        self.falls_ill(self.name, self.address)
+
+
+def call_doctor(name, address):
+    print(f'{name} needs a doctor at {address}')
+
+
+if __name__ == "__main__":
+    person = Person('Sherlock', '221B Baker St')
+    person.falls_ill.append(call_doctor)
+    person.falls_ill.remove(call_doctor)
+
+    person.catch_a_cold()
